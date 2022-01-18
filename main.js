e8766e23a07ed7cddc205f42727d7b887696765f
MSchwartz55 <mps520@nyu.edu>
2022-01-17 22:14:19 -0500

    added reset button

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
+let resetButton = document.getElementById("reset");
 let gridContainer = document.getElementById("container");
 
-function populateGrid(parent) {
-    for (let i = 0; i < 256; i++) {
+function populateGrid(parent, num) {
+    let size = num * num;
+    for (let i = 0; i < size; i++) {
         let div = document.createElement("div");
         div.classList.add("grid");
+        div.style.flex = `1 0 calc(${(1 / num) * 100}% - 4px)`;
         parent.appendChild(div);
     }
 }
 
-function addHover() {
-    let list = document.querySelectorAll('.grid');
+function addHoverEffect(selector) {
+    let list = document.querySelectorAll(selector);
 
     [].map.call(list, function (element) {
         element.addEventListener("mouseover", function (e) {
@@ -18,6 +21,22 @@ function addHover() {
     })
 }
 
-populateGrid(gridContainer);
-addHover();
+populateGrid(gridContainer, 16);
+addHoverEffect('.grid');
 
+resetButton.addEventListener('click', () => {
+    let response = window.prompt("How many squares would you like per row? (limit 100)");
+    if (response > 100 || response < 1) {
+        window.alert("invalid square count choice");
+        return;
+    }
+
+    let list = document.querySelectorAll(".grid");
+
+    [].map.call(list, function (element) {
+        element.remove();
+    })
+
+    populateGrid(gridContainer, response);
+    addHoverEffect(".grid");
+})
