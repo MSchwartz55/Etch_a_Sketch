68d148d22d5365624766d30814ad5238c3e4f1fd
MSchwartz55 <mps520@nyu.edu>
2022-01-17 21:32:32 -0500

    add hovering functionality

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-function populateGrid() {
-    let gridContainer = document.getElementById("container");
+let gridContainer = document.getElementById("container");
 
+function populateGrid(parent) {
     for (let i = 0; i < 256; i++) {
         let div = document.createElement("div");
         div.classList.add("grid");
-        gridContainer.appendChild(div);
+        parent.appendChild(div);
     }
 }
 
-populateGrid();
+function addHover() {
+    let list = document.querySelectorAll('.grid');
+
+    [].map.call(list, function (element) {
+        element.addEventListener("mouseover", function (e) {
+            e.target.style.backgroundColor = "pink";
+        })
+    })
+}
+
+populateGrid(gridContainer);
+addHover();
+
